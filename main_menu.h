1d44a27b0b92dfa7933d19e9e04ca978ab000ef6
Clement <clementdt99@gmail.com>
2017-10-21 16:58:34 +0200

    Ajout de l'affichage du menu principal sans les boutons

---
+++ main_menu.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_MENU_H_INCLUDED
+#define MAIN_MENU_H_INCLUDED
+
+void printResolutions(int screenIndex = 0);
+void displayBackground(SDL_Window** window);
+
+#endif // MAIN_MENU_H_INCLUDED
